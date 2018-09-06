92294b73378a8990918416da127198214af22a6f
RaphAlemoh <raphealenike@gmail.com>
2018-09-06 15:41:46 -0700

    First edition of my Portfolio

---
+++ index.php
@@ -0,0 +1,400 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Alemoh Rapheal Baja's Portfolio</title>
+		<style>
+.collapsible {
+    background-color: #777;
+    color: white;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+}
+
+.active, .collapsible:hover {
+    background-color:green;
+}
+
+.collapsible:after {
+    content: '\002B';
+    color: white;
+    font-weight: bold;
+    float: right;
+    margin-left: 5px;
+}
+
+.active:after {
+    content: "\2212";
+}
+
+.content {
+    padding: 0 18px;
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.2s ease-out;
+    background-color: #f1f1f1;
+	margin-bottom:0.2em;
+}
+</style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">   
+    <link rel="stylesheet" href="page.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500,600,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
+  </head>
+  <body>
+    <!-- Page Content Holder -->
+    <div class="container">
+      <!-- BEGIN NAVBAR -->
+      <div class="container">
+      <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                          data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">meetBaja.com</a>
+              </div >					
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  <ul class="nav navbar-nav navbar-right">
+      <li><a>Home<span class="sr-only">(current)</span></a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+    <li><a href="#education">Education</a></li>
+    <li><a href="#projects">Projects</a></li>
+    <li><a href="#skills">Skills</a></li>
+    <li> <a href="#contact">Contact</a></li>
+    </ul>
+
+                        </div>
+<!--try to make the toggle icon for this bar appear when it is on a small device-->
+                                            </div>
+                </nav>
+    </div>
+    <!-- END NAVBAR -->
+	
+     <!-- HEADER -->
+	 <div class="container">
+     <div class="jumbotron">
+        <div class="jumbotron-text">
+          <h5>Alemoh Rapheal Baja</h5>
+         </div>
+        <div class="jumbotron-social">
+          <ul class="ul-social">
+            <li class="li-social-links">
+              <a class="social" href="https://github.com/RaphAlemoh" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+            </li>
+            <li class="li-social-links">
+              <a class="social" href="www.linkedin.com/in/Alemoh-Rapheal-Baja" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            </li>
+            <li class="li-social-links">
+              <a class="social" href="https://www.facebook.com/rapheal.alemoh" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            </li>
+            <li class="li-social-links">
+              <a class="social" href="https://twitter.com/AlemsBaja" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </li>
+            <li class="li-social-links">
+              <a class="social" href="https://www.instagram.com/rapheal_alems/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>
+        <div id="down-arrow">
+          <span><a href="#about"><i class="fa fa-chevron-down" aria-hidden="true"></i></a></span>
+        </div>
+      </div>
+	  </div>
+      <!-- END HEADER -->
+	  
+	  	 <!-- BEGIN ABOUT -->
+	 <div class="container text-center">
+           <div class="content-header">
+            <a class="anchor" name="about"></a>
+            <h2>About</h2>
+          </div>
+          <div class="content-body">
+            <p>Alemoh Rapheal Baja is a web Developer, PHP programmer, ICT consultant, python enthusiast, and library specialist with 5yrs experience in Information Technology.. for more information check out my social channels</p>
+            </div>
+        </div>
+      <!-- END ABOUT -->
+ <!-- BEGIN EXPERIENCE -->
+ <div class="container">
+ <a class="anchor" name="experience"></a>
+            <h2>Experience</h2>
+ <div class="row">
+ <div class="col-lg-4 col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
+	<h3 class="panel-title">Raphnike ICT Service</h3>
+                         </div>
+						<div class="panel-body">
+						<div class="row">
+                                <div class="col-xs-6">
+                                   ICT Consultant
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <div class="huge">Rank:12</div>
+                                </div>
+                            </div>
+						</div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+              <div class="col-lg-4 col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
+	<h3 class="panel-title">RaphTecHub</h3>
+                         </div>
+						<div class="panel-body">
+						<div class="row">
+                                <div class="col-xs-6">
+                                   Programmer
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <div class="huge">Web Developer</div>
+                                </div>
+                            </div>
+						</div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+             <div class="col-lg-4 col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
+	<h3 class="panel-title">Website|Web Application</h3>
+                         </div>
+						<div class="panel-body">
+						<div class="row">
+                                <div class="col-xs-6">
+                                   Database Administrator
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <div class="huge">Content Manager</div>
+                                </div>
+                            </div>
+						</div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+ </div>
+      <!-- END EXPERIENCE -->
+
+          <!-- BEGIN EDUCATION -->
+<div class="container">
+<a class="anchor" name="education"></a>
+           <h2>Education</h2>
+<div class="row">
+<div class="col-lg-6 col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
+	<h3 class="panel-title">Udacity</h3>
+                         </div>
+						<div class="panel-body">
+						<div class="row">
+                                <div class="col-xs-6">
+                                   Front-End Developer
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <div class="huge">Developer</div>
+                                </div>
+                            </div>
+						</div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Google with Udacity and Andela Scholarship</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+               </div>
+               <div class="col-lg-6 col-md-6">
+                   <div class="panel panel-success">
+                        <div class="panel-heading">
+	<h3 class="panel-title">KEMS Global Concept</h3>
+                         </div>
+						<div class="panel-body">
+						<div class="row">
+                                <div class="col-xs-6">
+                                   ICT Student
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <div class="huge">2014</div>
+                                </div>
+                            </div>
+						</div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+               </div>
+           <!-- /.row -->
+</div>
+   </div><!-- END EDUCATION -->
+<!-- BEGIN PROJECTS -->
+<div class="container">
+ <div class="content-header">
+            <a class="anchor" name="projects"></a>
+            <h2>Projects</h2>
+          </div>
+<button class="collapsible">Library Managemnt System</button>
+<div class="content">
+  <p>Library Management System for managing the affairs of library administartion
+  <a href="#">Check my Git Repo</a>
+  </p>
+</div>
+<button class="collapsible">Search Script</button>
+<div class="content">
+  <p>Search for an item that is stored on the database
+  <a href="search-script.herokuapp.com">Check my Git Repo</a>
+  </p>
+</div>
+<button class="collapsible">Voting Web App</button>
+<div class="content">
+  <p>Web App for Voting candidates and also displays the result after the voting
+  <a href="#">Check my Git Repo</a>
+  </p>
+</div>
+
+<button class="collapsible">Field Trip Database</button>
+<div class="content">
+  <p>A database that keeps records of student payment for field trip and generate the sum total of students that have paid and the amount.
+  <a href="#">Check my Git Repo</a>
+  </p>
+</div>
+
+<button class="collapsible">Guess Game</button>
+<div class="content">
+  <p>The user enters a number and a number is generated randomly for the user to predict. If it matches with the number generated the user wins else the user tries for a number of time.
+  <a href="#">Check my Git Repo</a>
+  </p>
+</div>
+</div>
+      <!-- END PROJECTS -->
+
+      <!-- BEGIN SKILLS -->
+          <div class="container">
+            <a class="anchor" name="skills"></a>
+            <h2>Skills</h2>
+         <div class="row">
+		<div class="col-md-2"></div>
+				<div class="col-md-8">
+            <ul class="nav nav-pills">
+              <li><a>HTML</a></li>
+              <li><a>CSS</a></li>
+              <li><a>Bootstrap</a></li>
+              <li><a>PHP</a></li>
+              <li><a>Laravel</a></li>
+              <li><a>Database Admin</a></li>
+              <li><a>Javascript</a></li>
+                        </ul>
+						</div>
+						<div class="col-md-2"></div>
+          </div>
+ </div>
+      <!-- END SKILLS -->
+
+      <!-- BEGIN CONTACT -->
+	  <div class="container">
+	  <div class="row">
+	  <div class="content-header">
+            <a class="anchor" name="contact"></a>
+            <h2 class="white-text">Contact Me</h2>
+          </div>
+		  <div class="col-lg-3">
+		  </div>
+		  		  <div class="col-lg-6">
+		  <div class="text-center">
+                    <form method="POST" action="https://gmail.com/raphealenike@gmail.com" role="form">
+                <div class="form-group">
+				<input type="hidden" name="_subject" value="Contact request from personal website" />
+                              <label>Email:</label>
+                            <input type="email" class="form-control" name="_replyto" placeholder="Your email" required>
+							 <label for="name">Message</label>
+ <textarea class="form-control" rows="3" name="_replyto" placeholder="Your Message" required></textarea>
+                           </div> 
+						   <button type="submit" class="btn btn-default">Send</button>
+                    </form>
+				</div>
+				</div>
+						  <div class="col-lg-3">
+							</div>
+				</div>
+                </div>
+      <!-- END CONTACT -->
+
+      <!-- BEGIN FOOTER -->
+	  <br />
+        <div class="text-center text-success">
+            <p>Copyright @Alems_Baja 2018</p>
+          </div>
+		  <br />
+      <!-- END FOOTER -->
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+</script>
+
+    </div><!--closing container for the whole body-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.5.1/parsley.min.js"></script>
+
+  </body>
+</html>
